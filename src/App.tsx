87e0605fa5cd7afdeeb7ab6107f8b4f27ce7dad4
ana-web-ma <90105049+ana-web-ma@users.noreply.github.com>
2026-01-26 13:15:45 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className='darkbg-zinc-900 darktext-gray-100 flex h-screen justify-center bg-blue-200 text-gray-900'>
+      <section className='my-4 p-4'>template</section>
+    </main>
+  );
+}
+
+export default App;
