f2941a260f5d76bd8dac6eadc9e882be70bc7bfe
Ana-Ma-Web <90105049+Ana-Ma-Web@users.noreply.github.com>
2026-01-27 12:38:09 +0300

    feat: add comments button

--- src/modules/posts/Post.tsx
+++ src/modules/posts/Post.tsx
@@ -1,11 +1,18 @@
-import { Card, Stack } from 'react-bootstrap';
+import { Button, Card, Stack } from 'react-bootstrap';
 import { PostType } from './postsSlice';
+import { useState } from 'react';
 
 interface Props {
   post: PostType;
 }
 
 export default function Post(props: Props) {
+  const [showComments, setShowComments] = useState(false);
+
+  const toggleComments = () => {
+    setShowComments(!showComments);
+  };
+
   const { post } = props;
   return (
     <Card>
@@ -15,6 +22,10 @@ export default function Post(props: Props) {
           <Stack>
             <Card.Title>{post.title}</Card.Title>
             <Card.Text>{post.body}</Card.Text>
+            <Button className='me-auto mt-3' variant='outline-primary' size='sm' onClick={toggleComments}>
+              Comments
+            </Button>
+            {showComments && 'comments'}
           </Stack>
         </Stack>
       </Card.Body>
