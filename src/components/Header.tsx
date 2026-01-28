6057e5d3f50c32ab4583144d93689b213fad4135
Ana-Ma-Web <90105049+Ana-Ma-Web@users.noreply.github.com>
2026-01-28 17:35:56 +0300

    feat: add burger menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import { Button, Container, Nav, Navbar, Offcanvas } from 'react-bootstrap';
+import { Button, Container, Image, Nav, Navbar, Offcanvas, Stack } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
 export default function Header() {
   const [showMenu, setShowMenu] = useState(false);
@@ -18,14 +19,29 @@ export default function Header() {
           <span className='navbar-toggler-icon' />
         </Button>
         <Navbar.Offcanvas>
-          <Offcanvas show={showMenu} onHide={handleClose}>
-            <Offcanvas.Header closeButton>
-              <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+          <Offcanvas show={showMenu} onHide={handleClose} className='p-3'>
+            <Offcanvas.Header closeButton style={{ alignItems: 'flex-start' }}>
+              <Stack>
+                <Image
+                  style={{ maxWidth: '8rem' }}
+                  roundedCircle
+                  src='https://cdn.discordapp.com/attachments/652386576839475219/1463575869636477143/067a9be78604a82983c7c405f0c9e109.jpg?ex=697254bf&is=6971033f&hm=821cca2ebe2200a5d2edad867b0d9a75a5f10acb473f40ba78700e09e47b379a&'
+                />
+                <Offcanvas.Title>ana.web.ma@gmail.com</Offcanvas.Title>
+              </Stack>
             </Offcanvas.Header>
             <Offcanvas.Body>
-              <Nav className='justify-content-end flex-grow-1 pe-3'>
-                <Nav.Link href='#'>Home</Nav.Link>
-                <Nav.Link href='#'>About</Nav.Link>
+              <Nav activeKey='/' variant='string'>
+                <Nav.Item>
+                  <NavLink className={({ isActive }) => `${isActive ? 'text-dark' : 'text-primary'}`} to='/'>
+                    Home
+                  </NavLink>
+                </Nav.Item>
+                <Nav.Item>
+                  <NavLink className={({ isActive }) => `${isActive ? 'text-dark' : 'text-primary'}`} to='/about'>
+                    About
+                  </NavLink>
+                </Nav.Item>
               </Nav>
             </Offcanvas.Body>
           </Offcanvas>
