536f5153ce6826f8ec89bf663178ef2eafa83d38
Ana-Ma-Web <90105049+Ana-Ma-Web@users.noreply.github.com>
2026-01-28 10:09:30 +0300

    feat: add user route

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import Mainpage from 'pages/Mainpage';
 import { Route, Routes } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Userpage from 'pages/Userpage';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Layout />}>
         <Route index element={<Mainpage />} />
+        <Route path='user/:userId' element={<Userpage />} />
       </Route>
     </Routes>
   );
